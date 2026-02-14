eca66172697bd1802c8f86b82d046ce5dad8953d
Jiu10086 <afternoon19995@gmail.com>
2026-02-14 18:38:16 +0700

    fix requirement,updateplayer,init

--- epl/__init__.py
+++ epl/__init__.py
@@ -7,7 +7,7 @@ from epl.players.routes import players_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/epl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.db'
     app.secret_key = b'asjdlkdjlkasdjlkasjdlkjasdjasdlk'
 
     db.init_app(app)
